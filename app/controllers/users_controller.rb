b33ecfdacacca7e442c97c0c1b7d1b21210c72e7
malbazaz <39787850+malbazaz@users.noreply.github.com>
2018-08-17 11:14:27 -0400

    Define UsersController - new,create,view,index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,33 @@
 class UsersController < ApplicationController
+
+	def new
+        @user = User.new
+    end 
+    
+    def create 
+        binding.pry
+        @user= User.create(user_params)
+        session[:user_id]= @user.id
+    end 
+
+	def index
+        @users = User.all
+    end 
+
+
+    def show 
+        if !!session[:user_id]
+             @user = User.find_by_id(params[:id])
+        else 
+            redirect_to '/'
+        end
+    end 
+
+
+    private 
+
+    def user_params 
+        params.require(:user).permit(:fullname, :username, :password, :giftcoins, :admin, :email)
+    end 
+
 end
