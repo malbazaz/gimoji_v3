7c7e4ffc452fcfeccf703dd13c0023ee6f52c354
malbazaz <39787850+malbazaz@users.noreply.github.com>
2018-08-17 11:34:40 -0400

    Complete required migration (missing migration error.)

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_17_150200) do
+ActiveRecord::Schema.define(version: 2018_08_17_150621) do
 
   create_table "emotions", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "giftmoji_emotions", force: :cascade do |t|
+    t.integer "giftmoji_id"
+    t.integer "emotion_id"
   end
 
   create_table "giftmojis", force: :cascade do |t|
