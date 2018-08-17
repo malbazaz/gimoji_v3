d5983d432e1ebd3de657fad34034684a47969ac3
malbazaz <39787850+malbazaz@users.noreply.github.com>
2018-08-17 16:52:45 -0400

    Fix issue to save emotions in creating a new Giftmoji. Mainly in GiftmojiController - create.

--- app/controllers/giftmojis_controller.rb
+++ app/controllers/giftmojis_controller.rb
@@ -11,17 +11,33 @@ class GiftmojisController < ApplicationController
         if session[:user_id]
             @user = User.find_by_id(session[:user_id])
            
-            if @user.admin
-                render "new"
-            elsif @giftmoji.user_id == @user 
-                render "new" 
-            end 
+           # if @user.admin
+           #     render "new"
+           # elsif @giftmoji.user_id == @user 
+           #     render "new" 
+           # end 
         end 
     end
 
     def create 
+        #binding.pry
         @giftmoji = Giftmoji.create(giftmoji_params)
+        #binding.pry
+        if params[:giftmoji][:emotion_ids]
+        emotion_ids = params[:giftmoji][:emotion_ids]
+        emotion_ids.each do |em|
+           # binding.pry
+            if  em !=""
+                emotion = Emotion.find_by_id(em)
+                @giftmoji.emotions << emotion
+            end 
+        end 
+    end 
+        emo = Emotion.find_or_create_by(name: params[:giftmoji][:emotions][:name])
+        @giftmoji.emotions << emo
+        @giftmoji.save
         redirect_to "/giftmojis/#{@giftmoji.id}"
+ 
     end 
 
     def index 
@@ -50,14 +66,16 @@ class GiftmojisController < ApplicationController
     def update
     # need to update giftmoji by admin & update when gifted 
     #binding.pry
+    if params[:id]
         @giftmoji = Giftmoji.find_by_id(params[:id])
         @giftmoji.update(attr_params)
         @giftmoji.save 
         redirect_to "/giftmojis/#{@giftmoji.id}"
+    end
     end 
     private 
 
     def giftmoji_params 
-        params.require(:giftmoji).permit(:name, :tag, :message, :emotions, :occasion_id, :user_id)
+        params.require(:giftmoji).permit(:name, :tag, :price, :message, :occasion_id, :user_id)
     end 
 end
