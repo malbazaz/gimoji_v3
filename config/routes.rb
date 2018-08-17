fffed04e8526549681532d77c6cfd5532c87cec7
malbazaz <39787850+malbazaz@users.noreply.github.com>
2018-08-17 11:33:05 -0400

    Add index page and setup routes for index, login, logout, signup and users/:id

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,14 @@ Rails.application.routes.draw do
   resources :occasions
   resources :giftmojis
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :sessions
+
+get '/users/:id' => 'users#show'
+get '/' =>"application#index"
+get '/signup' => 'users#new'
+get 'login', to: 'sessions#new'
+post '/login' => 'sessions#create'
+get  '/logout' => 'sessions#destroy'
+post '/logout' => 'sessions#destroy'
+  #For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
