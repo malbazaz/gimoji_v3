49fb63ec29e04f620070a2abc067dad6d54975a8
malbazaz <39787850+malbazaz@users.noreply.github.com>
2018-08-17 12:13:57 -0400

    Add buy method for user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
          has_many :giftmojis
          has_many :giftmoji_emotions, through: :giftmojis 
          has_many :emotions, through: :giftmoji_emotions
+
+    def buy(gimoji)
+		if self.gift_coins >= giftmoji.price
+			self.gift_coins -= giftmoji.price 
+			giftmoji.user_id = self.id 
+			giftmoji.save 
+			self.save 
+		else 
+			"You don't have any enough giftcoins to purchase this giftmoji"
+		end 
+	end 
 end
