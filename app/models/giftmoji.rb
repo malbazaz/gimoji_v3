4eb8b67880b728ce72b84bfbf1ce7c9e13211303
malbazaz <39787850+malbazaz@users.noreply.github.com>
2018-08-17 12:14:55 -0400

    Add gift method to Giftmoji class.

--- app/models/giftmoji.rb
+++ app/models/giftmoji.rb
@@ -4,4 +4,18 @@ class Giftmoji < ApplicationRecord
 	has_many :giftmoji_emotions 
 	has_many :emotions, through: :giftmoji_emotions
 
+
+	def gift(recipient_name) 
+		if self.user_id == session[:user_id]
+			@user = User.find_by_fullname(recipient_name)
+  			self.user = @user
+  			self.save
+  		end 
+  	end 
+  end
+
+  # add class method of [most expensive giftmoji]
+  # add class of gimoji with most emotions
+  # add most gifts on what occasion 
+
 end
