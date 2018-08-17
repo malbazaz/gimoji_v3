11025a90b57bc138edc2b64eb77c1f3d58ce4c2a
malbazaz <39787850+malbazaz@users.noreply.github.com>
2018-08-17 11:11:40 -0400

    Add Bcrypt & Pry to Gemfile. Secure Password on User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+		 has_secure_password
          has_many :giftmojis
          has_many :giftmoji_emotions, through: :giftmojis 
          has_many :emotions, through: :giftmoji_emotions
