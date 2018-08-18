194b4fc898c8ba81221d47617d9e416d124850a2
malbazaz <39787850+malbazaz@users.noreply.github.com>
2018-08-18 11:11:11 -0400

    Add UID to users & Migrate. Correct error in omniauth initializer file.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_17_150621) do
+ActiveRecord::Schema.define(version: 2018_08_18_150837) do
 
   create_table "emotions", force: :cascade do |t|
     t.string "name"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2018_08_17_150621) do
     t.integer "giftcoins", default: 1000
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
