25bb4841323e9993f39fdcee4b5c30ae7a0a25d6
malbazaz <39787850+malbazaz@users.noreply.github.com>
2018-08-18 11:29:51 -0400

    Modify sessions controller to faciliate facebook login.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,21 +5,35 @@ class SessionsController < ApplicationController
 
   def create 
     #binding.pry
-  	 if params[:user][:username]
+  if auth['uid']
+    @user = User.find_or_create_by(uid: auth['uid']) do |u|
+      u.username = auth['info']['username']
+      u.email = auth['info']['email']
+    end 
+    session[:user_id] = @user.id 
+    redirect_to "/users/#{@user.id}"
+  end 
+
+
+  	elsif params[:user][:username]
     	@user = User.find_by(username: params[:user][:username])
-	   end  
-    #binding.pry
-	   if @user
-    	return redirect_to 'login' unless @user.authenticate(params[:user][:password])
-    	session[:user_id] = @user.id
-    	redirect_to "/users/#{@user.id}"
+	    if @user
+        return redirect_to 'login' unless @user.authenticate(params[:user][:password])
+        session[:user_id] = @user.id
+        redirect_to "/users/#{@user.id}"
       end
+    end
   end 
 
   def destroy
     session.delete :user_id 
     redirect_to '/'
   end 
+
+private 
+  def auth 
+    request.env['omniauth.auth']
+  end
 
 
 
