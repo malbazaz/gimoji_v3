19327a23e62a317b7b8db53983eedb13e188111a
malbazaz <39787850+malbazaz@users.noreply.github.com>
2018-08-18 23:42:11 -0400

    Add further validation to view giftcoins balance. Add new helper for current_user?

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -10,6 +10,10 @@ module UsersHelper
     	session[:user_id]
     end 
 
+    def current_user?
+        session[:user_id] == params[:id]
+    end
+
     def user_name
     	User.find_by_id(session[:user_id]).fullname
     end 
