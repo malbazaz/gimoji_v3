8fbc7030bb8b41e23ed1657d7636fbd9c6a464d2
malbazaz <39787850+malbazaz@users.noreply.github.com>
2018-08-19 23:23:12 -0400

    Add method into url.

--- app/helpers/occasions_helper.rb
+++ app/helpers/occasions_helper.rb
@@ -1,2 +1,12 @@
 module OccasionsHelper
+	include Rails.application.routes.url_helpers
+
+	class << self 
+
+	def most_popular_occasion
+		Occasion.joins(:giftmojis).group("giftmojis.id").order("count(giftmojis.id) ASC").limit(1).pluck(:name)
+		#occasion_path(self)
+	end
+
+end 
 end
--- app/models/occasion.rb
+++ app/models/occasion.rb
@@ -1,9 +1,12 @@
 class Occasion < ApplicationRecord
 		has_many :giftmojis
 		validates :name, presence: true 
+		#delegate :url_helpers, to: 'Rails.application.routes'
+		include Rails.application.routes.url_helpers
 
 	def self.most_popular_occasion
-		Occasion.joins(:giftmojis).group("giftmojis.id").order("count(giftmojis.id) ASC").limit(1)
+		Occasion.joins(:giftmojis).group("giftmojis.id").order("count(giftmojis.id) ASC").limit(1).pluck(:name)[0]
 	end
+
 
 end
