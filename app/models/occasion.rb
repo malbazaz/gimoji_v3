c2734ddca3fe61470eaff10678012d2a621661f0
malbazaz <39787850+malbazaz@users.noreply.github.com>
2018-08-19 21:39:43 -0400

    Add spec.md & add class method.

--- app/models/occasion.rb
+++ app/models/occasion.rb
@@ -3,7 +3,7 @@ class Occasion < ApplicationRecord
 		validates :name, presence: true 
 
 	def self.most_popular_occasion
-		##
+		Occasion.joins(:giftmojis).group("giftmojis.id").order("count(giftmojis.id) ASC").limit(1)
 	end
 
 end
