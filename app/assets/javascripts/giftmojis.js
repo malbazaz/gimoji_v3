911806680038a94bcbabd98a8b4a5e55f12670fb
malbazaz <39787850+malbazaz@users.noreply.github.com>
2018-09-09 21:56:49 -0400

    Create JS object, prototype function and replace code in Index and New shows with function.

---
+++ app/assets/javascripts/giftmojis.js
@@ -0,0 +1,24 @@
+function Giftmoji(name, tag, price, emotions){
+	this.name = name
+	this.tag = tag 
+	this.price = price 
+	this.emotions = emotions
+}
+
+ Giftmoji.prototype.emotionsPrint = function() {
+ 	let emoArray = this.emotions 
+ 	let emoPrint = ""
+  for(let i=0;i<emoArray.length;i++){
+  emoPrint += emoArray[i]["name"]
+  if(i<emoArray.length-1){
+  emoPrint += ", "
+} else 
+{
+	emoPrint += ". "
+  }
+
+}
+    	  
+return emoPrint
+      }
+
